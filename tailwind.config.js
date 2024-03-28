7177d932334615e3691329df18ff24ec4e251dce
tusharm1204 <tusharm.octal8@gmai.com>
2024-03-28 10:20:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{vue,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#0ea5e9',
+      },
+    },
+  },
+  plugins: [],
+}
